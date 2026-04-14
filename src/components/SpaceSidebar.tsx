7ed28031c1f3243874085f01b90231e73995377d
Carter <carterghill@gmail.com>
2026-04-13 19:41:41 -0600

    bigger home button logo

--- src/components/SpaceSidebar.tsx
+++ src/components/SpaceSidebar.tsx
@@ -256,7 +256,7 @@ export default function SpaceSidebar({
             display: "flex",
             alignItems: "center",
             justifyContent: "center",
-            padding: 8,
+            padding: 5,
           }}
         >
           <img
